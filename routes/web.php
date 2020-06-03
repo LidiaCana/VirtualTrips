b2c9190be428a6f93e158d2fb04f64c7d2cd0e2c
¨Lidia¨ <in.canaless@gmail.com>
2020-06-03 00:22:40 -0600

    welcome

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\trips;
 
 Route::get('/', function () {
 
- return view('welcome');
+    $data = trips::latest()->paginate(5);
+    return view('welcome', compact('data'))
+    ->with('i', (request()->input('page', 1) - 1) * 5);
 });
 
 Auth::routes();
